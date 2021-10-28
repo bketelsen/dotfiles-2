81b9237a5bf7030d98ea4804579657053f2c8b62
Brian Ketelsen <bketelsen@gmail.com>
2021-10-28 01:08:27 -0400

    Update common.zsh

--- .zsh/includes/common.zsh
+++ .zsh/includes/common.zsh
@@ -3,3 +3,12 @@ zinit light zdharma/fast-syntax-highlighting
 
 zinit ice wait lucid atload"_zsh_autosuggest_start"
 zinit light zsh-users/zsh-autosuggestions
+
+# https://stackoverflow.com/questions/6569478/detect-if-executable-file-is-on-users-path
+function is_bin_in_path {
+  if [[ -n $ZSH_VERSION ]]; then
+    builtin whence -p "$1" &> /dev/null
+  else  # bash:
+    builtin type -P "$1" &> /dev/null
+  fi
+}
