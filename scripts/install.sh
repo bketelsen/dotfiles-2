33e53a1eb990ae9bedccd2c932719aff99027810
Brian Ketelsen <bketelsen@gmail.com>
2021-11-04 05:10:00 -0400

    eget tools

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,3 +8,20 @@ ln -s $HOME/dotfiles/.zshrc ~/.zshrc
 ln -s $HOME/dotfiles/.zprofile ~/.zprofile
 ln -s $HOME/dotfiles/.bashrc ~/.bashrc
 ln -s $HOME/dotfiles/.zsh ~/.zsh
+
+curl -o eget.sh https://zyedidia.github.io/eget.sh
+shasum -a 256 eget.sh # verify with hash below
+bash eget.sh
+sudo cp eget /usr/local/bin/
+
+mkdir -p $HOME/bin
+cd $HOME/bin
+eget bootandy/dust
+eget muesli/duf
+eget sharkdp/bat
+eget sharkdp/fd
+eget BurntSushi/ripgrep
+eget junegunn/fzf
+eget stdolan/jq
+eget dalance/procs
+eget ogham/dog
