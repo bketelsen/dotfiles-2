ff29b4368dbf195fe073a4d884ddaea71563dedd
Brian Ketelsen <bketelsen@gmail.com>
2023-09-07 14:35:35 +0000

    fix: who knows

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-mv ~/.bashrc ~/.bashrc.bak
-mv ~/.profile ~/.profile.bak
+# mv ~/.bashrc ~/.bashrc.bak
+# mv ~/.profile ~/.profile.bak
 
-ln -s $HOME/dotfiles/.bashrc ~/.bashrc
-ln -s $HOME/dotfiles/.profile ~/.profile
-ln -s $HOME/dotfiles/.gitconfig ~/.gitconfig
+# ln -s $HOME/dotfiles/.bashrc ~/.bashrc
+# ln -s $HOME/dotfiles/.profile ~/.profile
+# ln -s $HOME/dotfiles/.gitconfig ~/.gitconfig
 
 sudo apt-get install -y build-essential
 export NONINTERACTIVE=1
